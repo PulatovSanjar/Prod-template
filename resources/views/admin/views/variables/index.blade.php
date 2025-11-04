b2f18f5f759330891f6cc2ead96424b42d572b02
SanjarPulatov <sanjarovs123@gmail.com>
2025-11-04 22:13:28 +0500

    feature: created new menu panel

--- resources/views/admin/views/variables/index.blade.php
+++ resources/views/admin/views/variables/index.blade.php
@@ -35,7 +35,6 @@
                         </tbody>
                     </table>
                 </div>
-                @include('admin.helpers.save_buttons')
             </form>
         </div>
     </div>
